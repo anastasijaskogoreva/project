9b8107dfdf4aa6c1b584ebc7579a851e82cb7a5f
Anastasija Skogoreva <144266164+anastasijaskogoreva@users.noreply.github.com>
2024-01-17 20:57:03 +0200

    code_for_car.py

---
+++ code_for_car.py
@@ -0,0 +1,32 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+service = Service()
+option = webdriver.ChromeOptions()   # internet brauzeris
+driver = webdriver.Chrome(service=service, options=option)
+
+url = "https://www.ss.lv/lv/transport/cars/"  # saite
+driver.get(url)
+time.sleep(2)
+
+find=driver.find_element(By.ID, "ahc_166")     # mašīnas marka
+find.click()
+
+find=driver.find_element(By.CLASS_NAME, "a19")  # jauni sludinajumi
+find.click()
+time.sleep(2)
+
+find=driver.find_element(By.ID,"f_o_8_min")  # min cena
+find.send_keys("1000")  # kāda min cena
+find=driver.find_element(By.ID,"f_o_8_max")  # max cena
+find.send_keys("9000")  # kāda max cena
+time.sleep(2)
+
+find=driver.find_element(By.CLASS_NAME, "b.s12") # meklēt 
+find.click()
+time.sleep(2)
+
+input()
